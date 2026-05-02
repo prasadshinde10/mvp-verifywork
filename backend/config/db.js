1bffa55c893c8a8e95f380698b3485bd1c391975
prasad shinde <154340812+prasadshinde10@users.noreply.github.com>
2026-05-02 20:16:09 +0530

    Update SSL configuration in db.js
    Set rejectUnauthorized to false for SSL configuration.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: process.env.PG_SSL_REJECT_UNAUTHORIZED !== 'false',
-  },
+  rejectUnauthorized: false
+},
 });
 
 pool
